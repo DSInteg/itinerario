d1cc4a8c7585291ab075a82196b99b9cc7e771a6
Juan Jose Cordova Zamorano <jcordova@dsinteg.com>
2020-03-09 20:20:11 -0600

    Primera plantilla
    Prueba de drag and drop que nos permita almacenar en firebase los movimientos de ordenamiento

---
+++ js/app.js
@@ -0,0 +1,11 @@
+(function(){
+      // Your web app's Firebase configuration
+    
+      const preObject = document.getElementById('objeto');
+      
+      // crea referencia a Firebase
+      //const dbRefObject = firebase.database().ref().child('objeto');
+      //sincroniza con Firebase
+     //dbRefObject.on('value', snap => console.log(snap.val()));
+
+})
